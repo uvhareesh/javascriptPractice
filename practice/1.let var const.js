8bccc42bd3868e04ed139e1b2df6f1d026063662
uvh127 <uvharish90@gmail.com>
2025-04-17 13:50:30 +0530

    practice1 Var vs Let vs Const file

---
+++ practice/1.let var const.js
@@ -0,0 +1,31 @@
+var a = 5;
+console.log(a);
+
+
+{
+let b=5;
+}
+console.log(b);
+
+{
+let b=5;
+console.log(b);
+}
+
+
+{
+
+    const c=5;
+}
+
+console.log(c);
+
+{
+    const c=5;
+    console.log(c);
+}
+
+
+
+
+
